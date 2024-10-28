d9ea762c0d4504e610a8ecc0d0994f0f6282224f
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-28 16:58:18 +0100

    Add return type to function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """0-simple_helper_function"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int):
+def index_range(page: int, page_size: int) -> Tuple:
     """
     return a tuple of size two containing a start index
     and an end index corresponding to the range of indexes
