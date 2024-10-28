bf36dd72f8a35c0a3ad9c204f8ce3a8a30f42a3d
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-28 19:18:43 +0100

    Add docstring to the method

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -24,6 +24,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        return the appropriate page of the dataset
+        (i.e. the correct list of rows).
+        """
         # Validate input
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
