f1e4f041d3da178506f320e0589173d021cf9d05
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-02 16:15:11 +0100

    Create LRUCache that inherit from BaseCaching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""3-lru_cache module """
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """ Inherit from BaseCaching """
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # Remove the least recently used item (LRU)
+            print('put-before', self.cache_data)
+            first_key = self.cache_data.popitem(last=False)[0]
+            print('put-after', self.cache_data)
+            print("DISCARD: {}".format(first_key))
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is not None and key in self.cache_data:
+            print('get-before', self.cache_data)
+            self.cache_data.move_to_end(key)
+            print('get-after', self.cache_data)
+            return self.cache_data.get(key)
+        return None
