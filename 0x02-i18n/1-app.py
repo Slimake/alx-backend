0ffdb4b7597af313847e4077d3ce756dfbe22ba9
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-05 17:44:57 +0100

    Refactor code

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,18 +2,15 @@
 """1-app Module
 """
 from flask import Flask, render_template
-from flask_babel import Babel
-
-# Instantiate the Babel object
-babel = Babel()
+from flask_babel import Babel, _
 
 
 class Config:
     """Config class for babel
     """
     LANGUAGES = ["en", "fr"]
-    LANG_DEFAULT = "en"
-    TIMEZONE_DEFAULT = "UTC"
+    BABEL_DEFAULT_LOCALE = "es"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 # Flask app instance
@@ -22,4 +19,12 @@ app = Flask(__name__)
 # Set app configuration from the Config class
 app.config.from_object(Config)
 
-babel.init_app(app)
+# Instantiate the Babel object
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """Homepage
+    """
+    return render_template('1-index.html')
