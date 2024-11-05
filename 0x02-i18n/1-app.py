4cf9fbaabdc6c7fe23e84a4b59ef862c555d9c3f
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-05 16:45:41 +0100

    Modify code

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,19 +4,25 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+# Instantiate the Babel object
+babel = Babel()
+
 
 class Config:
+    """Config class for babel
+    """
     LANGUAGES = ["en", "fr"]
+    LANG_DEFAULT = "en"
+    TIMEZONE_DEFAULT = "UTC"
 
 
 # Flask app instance
 app = Flask(__name__)
 
-# Instantiate the Babel object
-babel = Babel(app)
-
 # Set app configuration from the Config class
 app.config.from_object(Config)
+
+babel.init_app(app)
 
 
 @app.route("/")
