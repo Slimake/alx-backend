53c32e23a9f1212efa9e7da2bbf66ab2efcbb10e
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-12-04 18:06:02 +0100

    Use publisher to process jobs

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,18 @@
+import { createClient, print } from "redis";
+
+const subscriber = createClient();
+subscriber
+  .on('error', err => console.log('Redis client not connected to the server: Error:', err.message))
+  .on('connect', () => console.log('Redis client connected to the server'));
+
+  // Subscribe to a channel
+subscriber.subscribe('holberton school channel');
+
+// Listen for messages
+subscriber.on('message', (channel, message) => {
+  console.log(message);
+  if (message == 'KILL_SERVER') {
+    subscriber.unsubscribe('holberton school channel');
+    subscriber.quit();
+  }
+});
