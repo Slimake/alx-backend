0300522a5541e6925cb9b7202794a04fe7289b9c
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-12-05 00:22:03 +0100

    Add semi-colon, use correct name for queue

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -5,15 +5,15 @@ const queue = createQueue();
 const jobData = {
   phoneNumber: '07035681098',
   message: 'Hello there',
-}
+};
 
-const push_notification_code = queue.create('jobData', jobData).save((err) => {
+const job = queue.create('push_notification_code', jobData).save((err) => {
   if (!err) {
-    console.log(`Notification job created: ${push_notification_code.id}`);
+    console.log(`Notification job created: ${job.id}`);
   }
 });
 
-push_notification_code.on('job complete', (id) => {
+job.on('job complete', (id) => {
   console.log('Notification job completed');
 })
 .on('job failed', (id, err) => {
