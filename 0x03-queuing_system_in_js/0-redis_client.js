94712dde640a0604c73b7bfe496c947fbdd72bf4
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-12-04 13:41:22 +0100

    No use for the function

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,10 +1,7 @@
 import { createClient } from "redis";
 
 const client = createClient();
-function nodeRedis() {
-  client
-    .on('error', err => console.log('Redis client not connected to the server: Error:', err.message))
-    .on('connect', () => console.log('Redis client connected to the server'));
-}
-
-nodeRedis();
+client
+  .on('error', err => console.log('Redis client not connected to the server: Error:', err.message))
+  .on('connect', () => console.log('Redis client connected to the server'));
+  
